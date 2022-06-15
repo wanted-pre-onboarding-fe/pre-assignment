a69be83c022ce384378501cfd44fbdf740eb0aba
corgi-world <developer.msw@gmail.com>
2022-06-15 21:40:14 +0900

    GNB UI 구현

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,19 +1,110 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { Outlet } from 'react-router';
+import { Outlet } from 'react-router-dom';
+
+import {
+  MdHome,
+  MdOutlineNearMe,
+  MdOutlineAddBox,
+  MdOutlineExplore,
+  MdFavoriteBorder,
+  MdLogout,
+} from 'react-icons/md';
 
 export default function Navigation() {
   return (
     <>
-      <Contianer></Contianer>
+      <Wrapper>
+        <Nav>
+          <Logo src="/images/logo.png" />
+          <Search placeholder="검색" />
+          <IconsWrapper>
+            <Button>
+              <MdHome size="29" />
+            </Button>
+            <Button>
+              <MdOutlineNearMe size="29" />
+            </Button>
+            <Button>
+              <MdOutlineAddBox size="29" />
+            </Button>
+            <Button>
+              <MdOutlineExplore size="29" />
+            </Button>
+            <Button>
+              <MdFavoriteBorder size="29" />
+            </Button>
+            <Button>
+              <MdLogout size="29" />
+            </Button>
+          </IconsWrapper>
+        </Nav>
+      </Wrapper>
       <Outlet />
     </>
   );
 }
 
-const Contianer = styled.div`
-  width: 100vw;
-  height: 30vh;
-  background-color: lightgray;
+const Wrapper = styled.div`
+  width: 100%;
+  height: 60px;
+  top: 0;
+  position: sticky;
+  display: flex;
+  justify-content: center;
+  border-bottom: 1px solid lightgray;
+`;
+
+const Nav = styled.nav`
+  width: 975px;
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 20px;
+  padding-right: 20px;
+  @media (max-width: 400px) {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+`;
+
+const Logo = styled.img`
+  height: 50px;
+  position: relative;
+  top: 3px;
+`;
+
+const Search = styled.input`
+  width: 265px;
+  height: 35px;
+  background-color: #efefef;
+  border-radius: 5px;
+  padding: 10px;
+  font-size: 15px;
+
+  @media (max-width: 720px) {
+    max-width: 265px;
+    width: 50%;
+    margin-left: 20px;
+    margin-right: 20px;
+  }
+
+  @media (max-width: 620px) {
+    display: none;
+  }
+`;
+
+const IconsWrapper = styled.div`
+  width: 254px;
+  min-width: 254px;
+  height: 35px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Button = styled.div`
+  cursor: pointer;
 `;
