f1c134631dd3ebce3bd35defa5c82ddcd6f01db0
corgi-world <developer.msw@gmail.com>
2022-06-15 13:39:12 +0900

    validate 함수명 소문자로 변경

--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -3,11 +3,11 @@ const RegExEmail =
 const RegExPassword =
   /^(?=.*[A-Z])(?=.*\d)(?=.*[#?!@$%^&*-])[A-Za-z\d#?!@$%^&*-]{8,}$/;
 
-export function ValidateEmail(email) {
+export function validateEmail(email) {
   if (!email) return null;
   return RegExEmail.test(email);
 }
-export function ValidatePassword(password) {
+export function validatePassword(password) {
   if (!password) return null;
   return RegExPassword.test(password);
 }
