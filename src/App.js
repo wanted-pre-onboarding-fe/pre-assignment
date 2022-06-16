468a56eee29e439740395acb3df087c295b12bf5
Marco <jonghyeok.lim14@gmail.com>
2022-06-16 21:31:55 +0900

    GNB퍼블리싱

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import GNB from './components/common/GNB';
 import Login from './pages/login/Login';
 
 function App() {
-  return <Login />;
+  return <GNB />;
 }
 
 export default App;
--- src/components/common/GNB.js
+++ src/components/common/GNB.js
@@ -1,7 +1,57 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const GNB = () => {
-  return <div>hellow GNB</div>;
+  return (
+    <StyledGNB>
+      <StyledNav>
+        <StyledInstaLogo alt="logo" src="instagram_logo.png" />
+        <StyledSearch type="text" placeholder="🔍 &nbsp; 검색" />
+        <StyledNavIcon>
+          <StyledIcon className="fa-solid fa-house"></StyledIcon>
+          <StyledIcon className="fa-regular fa-paper-plane"></StyledIcon>
+          <StyledIcon className="fa-regular fa-square-plus"></StyledIcon>
+          <StyledIcon className="fa-regular fa-compass"></StyledIcon>
+          <StyledIcon className="fa-regular fa-heart"></StyledIcon>
+          <StyledIcon className="fa-solid fa-circle-user"></StyledIcon>
+        </StyledNavIcon>
+      </StyledNav>
+    </StyledGNB>
+  );
 };
+
+const StyledGNB = styled.header`
+  position: sticky;
+  height: 40px;
+  border: 1px solid #eaeaea;
+  align-items: center;
+`;
+
+const StyledNav = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  max-width: 1800px;
+  padding: 5px;
+`;
+
+const StyledInstaLogo = styled.img`
+  width: 100px;
+`;
+
+const StyledSearch = styled.input`
+  width: 200px;
+  border-radius: 5px;
+  background-color: #efefef;
+`;
+
+const StyledNavIcon = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const StyledIcon = styled.i`
+  margin: 0 10px;
+  font-size: 20px;
+`;
 
 export default GNB;
