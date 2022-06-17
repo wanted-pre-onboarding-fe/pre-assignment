988b900182b15b5d401721e982da10e9358254a7
yunjink <chloeeekim1109@gmail.com>
2022-06-18 01:58:46 +0900

    로고 이미지 추가 및 globalStyles 설정

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -3,11 +3,27 @@ import reset from 'styled-reset';
 
 const GlobalStyles = createGlobalStyle`
  ${reset}
+
  * {
-   box-sizing:border-box;
-   outline:none;
-   border:none;
+    box-sizing: border-box;
+    outline: none;
+    margin: 0;
+    padding: 0;
  }
+
+ body {
+    width: 100%;
+    background: #fafafa;
+    position: relative;
+ }
+  
+  button {
+    border: none;
+    cursor: pointer;
+    outline: none;
+  }
+  
+
 `;
 
 export default GlobalStyles;
