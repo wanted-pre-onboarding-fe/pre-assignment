17cbd4741df082dea0ea76c498f14d0695d80675
corgi-world <developer.msw@gmail.com>
2022-06-17 14:16:45 +0900

    gh pages 배포 준비

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyles />
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
