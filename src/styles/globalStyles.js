76be4ccb87a623128f4a61816e33581b19df1952
Eunseo Kim <kim.eunseo@kakao.com>
2022-06-22 23:39:51 +0900

    globalStyles 수정

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -8,6 +8,19 @@ const GlobalStyles = createGlobalStyle`
    outline:none;
    border:none;
  }
+
+ html, body {
+  height : 100vh;
+}
+
+ul {
+  list-style: none;
+}
+
+a {
+  color: black;
+  text-decoration: none;
+}
 `;
 
 export default GlobalStyles;
