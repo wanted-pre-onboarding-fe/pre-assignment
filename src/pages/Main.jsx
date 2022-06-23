100a468c6338be24b39cfdb1e9e1c92328ad566e
sunaerocket <sunaerocket@gmail.com>
2022-06-23 16:38:03 +0900

    메인페이지 미디어쿼리 브레이크 포인트 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -30,4 +30,21 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   margin: 0 auto;
+  /* 
+  * 미디어쿼리는 별도 지시 사항이 없어서 브레이크포인트만 잡아둠
+  */
+  @media (max-width: 320px) {
+  }
+  @media (max-width: 375px) {
+  }
+  @media (max-width: 425px;) {
+  }
+  @media (max-width: 768px;) {
+  }
+  @media (max-width: 1024px;) {
+  }
+  @media (max-width: 1440px;) {
+  }
+  @media (max-width: 2560px;) {
+  }
 `;
