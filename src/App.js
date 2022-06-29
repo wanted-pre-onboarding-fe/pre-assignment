662c2f5c6a7063f72202a44d64e4533d66881cde
bebusl <bebus1998@naver.com>
2022-06-29 18:20:42 +0900

    과제4 해결
    * 로그인 페이지에서 로그인 성공 시 main페이지로
    * 로그아웃 페이지에서 로그아웃 성공 시 로그인페이지로 이동:

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import PreAssignmentGuide from './pages/PreAssignmentGuide';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/layout';
+import Header from './components/layout/Header';
+import Login from './pages/Login';
+import Main from './pages/Main';
 
 function App() {
-  return <PreAssignmentGuide />;
+  return (
+    <Layout>
+      <Routes>
+        <Route index element={<Login />}></Route>
+
+        <Route path="main" element={<Main />}></Route>
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
